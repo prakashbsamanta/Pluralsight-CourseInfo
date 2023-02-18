f7cf106e42818e6b12c3f97d4887b8de79a87fca
prakash.s <prakash.s@captainfresh.in>
2023-02-18 12:00:21 +0530

    project started running

---
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -0,0 +1,8 @@
+package com.pluralsight.courseinfo.cli;
+
+public class CourseRetriever {
+    public static void main(String[] args) {
+        System.out.println("Came here");
+        System.out.println("new project worked");
+    }
+}
