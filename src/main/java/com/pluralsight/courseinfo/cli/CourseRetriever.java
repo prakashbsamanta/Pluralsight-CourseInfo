2b6001898ee70304a2e7b30e51e9a909544ab429
prakash.s <prakash.s@captainfresh.in>
2023-02-19 21:36:04 +0530

    slf4j logging added

--- src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
+++ src/main/java/com/pluralsight/courseinfo/cli/CourseRetriever.java
@@ -1,8 +1,27 @@
 package com.pluralsight.courseinfo.cli;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class CourseRetriever {
-    public static void main(String[] args) {
-        System.out.println("Came here");
-        System.out.println("new project worked");
+    public static final Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
+
+    public static void main(String... args) {
+        LOG.info("Course Retriever is Starting!!!");
+
+        if (args.length == 0) {
+            LOG.warn("Please Provide an author name as first argument");
+            return;
+        }
+
+        try {
+            retrieveCourses(args[0]);
+        } catch (Exception e) {
+            LOG.error("Unexpected exception: ", e);
+        }
+    }
+
+    public static void retrieveCourses(String authorId) {
+        LOG.info("Retrieving author courses: '{}'", authorId);
     }
 }
